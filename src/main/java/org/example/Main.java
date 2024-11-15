c84235a19581b42f52e93fe0b552db3b7c1f3bfb
jcanaviri42 <jcanaviri42@gmail.com>
2024-11-15 18:15:49 -0400

    transaction history example

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,35 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to JavaBank ATM! Version control with GIT.");
+        double balance = 1000.0;
+        ArrayList<String> transactionHistory = new ArrayList<>();
+
+        balance = deposit(500, balance, transactionHistory);
+        balance = withdraw(100, balance, transactionHistory);
+
+        System.out.println("Balance final: $" + balance);
+        for (String transaction : transactionHistory)
+            System.out.println(transaction);
+    }
+
+    public static double deposit(double amount, double balance, ArrayList<String> transactionHistory) {
+        if (amount > 0) {
+            transactionHistory.add("Deposited: $" + amount);
+            return balance + amount;
+        }
+        return balance;
+    }
+
+    public static double withdraw(double amount, double balance, ArrayList<String> transactionHistory) {
+        if (balance >= amount) {
+            balance -= amount;
+            transactionHistory.add("Withdrew: $" + amount);
+            return balance;
+        }
+        System.out.println("Insufficient funds");
+        return balance;
     }
 }
